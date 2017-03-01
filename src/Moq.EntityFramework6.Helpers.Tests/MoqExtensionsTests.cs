d7b5dc78e94fa77ed13b5ff3a95040f7ab9e2fff
Michał Jankowski <jankowski.m@gmail.com>
2017-03-01 21:57:28 +0100

    Added tests for MoqExtensions.cs

---
+++ src/Moq.EntityFramework6.Helpers.Tests/MoqExtensionsTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Moq.EntityFramework6.Helpers.Tests
+{
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using Xunit;
+
+    public class MoqExtensionsTests
+    {
+        private readonly Mock<UsersContext> objectsContextMock = new Mock<UsersContext>();
+
+        [Fact]
+        public void Given_DBContextMock_When_MockingDBSet_Then_NormalQueryShouldWork()
+        {
+            // Arrange
+            this.objectsContextMock.Setup(x => x.Users).Returns(GenerateUsers());
+
+            // Act
+            List<User> result = this.objectsContextMock.Object.Users.ToList();
+
+            // Assert
+            Assert.Equal(4, result.Count);
+        }
+
+        [Fact]
+        public async void Given_DBContextMock_When_MockingDBSet_Then_AsyncQueryShouldWork()
+        {
+            // Arrange
+            this.objectsContextMock.Setup(x => x.Users).Returns(GenerateUsers());
+
+            // Act
+            List<User> result = await this.objectsContextMock.Object.Users.ToListAsync();
+
+            // Assert
+            Assert.Equal(4, result.Count);
+        }
+
+        private static IEnumerable<User> GenerateUsers()
+        {
+            return new List<User> {new User(), new User(), new User(), new User()};
+        }
+
+        public class UsersContext : DbContext
+        {
+            public virtual DbSet<User> Users { get; set; }
+        }
+
+        public class User
+        {
+            public int Id { get; set; }
+            public string Login { get; set; }
+        }
+    }
+}
