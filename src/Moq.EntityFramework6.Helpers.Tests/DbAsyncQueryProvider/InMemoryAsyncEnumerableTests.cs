32a5ca922319e4f5de41bcdce63a8e29e20e7b0f
Michał Jankowski <jankowski.m@gmail.com>
2017-03-01 19:03:24 +0100

    Added unit tests for DbAsyncQueryProvider

---
+++ src/Moq.EntityFramework6.Helpers.Tests/DbAsyncQueryProvider/InMemoryAsyncEnumerableTests.cs
@@ -0,0 +1,83 @@
+﻿namespace Moq.EntityFramework6.Helpers.Tests.DbAsyncQueryProvider
+{
+    using System.Collections.Generic;
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Helpers.DbAsyncQueryProvider;
+    using Xunit;
+
+    public class InMemoryAsyncEnumerableTests
+    {
+        [Fact]
+        public void
+            Given_InMemoryAsyncEnumerable_When_GetAsyncEnumerator_Then_EnumeratorFromInInnerEnumerableShouleBeUsed()
+        {
+            // Arrange
+            var enumerableMock = new Mock<IEnumerable<int>>();
+            var inMemoryAsyncEnumerable = new InMemoryAsyncEnumerable<int>(enumerableMock.Object);
+
+            // Act
+            inMemoryAsyncEnumerable.GetAsyncEnumerator();
+
+            // Assert
+            enumerableMock.Verify(x => x.GetEnumerator());
+        }
+
+        [Fact]
+        public void
+            Given_InMemoryAsyncEnumerable_When_GetAsyncEnumeratorFromInterface_Then_EnumeratorFromInInnerEnumerableShouleBeUsed
+            ()
+        {
+            // Arrange
+            var enumerableMock = new Mock<IEnumerable<int>>();
+            var inMemoryAsyncEnumerable = new InMemoryAsyncEnumerable<int>(enumerableMock.Object);
+
+            // Act
+            ((IDbAsyncEnumerable) inMemoryAsyncEnumerable).GetAsyncEnumerator();
+
+            // Assert
+            enumerableMock.Verify(x => x.GetEnumerator());
+        }
+
+        [Fact]
+        public void Given_InMemoryAsyncEnumerable_When_GetProviderFromInterface_Then_CorrectProviderIsReturned()
+        {
+            // Arrange
+            var enumerableMock = new Mock<IEnumerable<int>>();
+            var inMemoryAsyncEnumerable = new InMemoryAsyncEnumerable<int>(enumerableMock.Object);
+
+            // Act
+            var provider = ((IQueryable) inMemoryAsyncEnumerable).Provider;
+
+            // Assert
+            Assert.IsType<InMemoryAsyncQueryProvider<int>>(provider);
+        }
+
+        [Fact]
+        public void Given_Enumerable_When_ObjectCreated_Then_ObjectCorrectyBuild()
+        {
+            // Arrange
+            var enumerableMock = new Mock<IEnumerable<int>>();
+
+            // Act
+            var inMemoryAsyncEnumerable = new InMemoryAsyncEnumerable<int>(enumerableMock.Object);
+
+            // Assert
+            Assert.Equal(enumerableMock.Object.GetEnumerator(), ((IEnumerable<int>)inMemoryAsyncEnumerable).GetEnumerator());
+        }
+
+        [Fact]
+        public void Given_Expression_When_ObjectCreated_Then_ObjectCorrectyBuild()
+        {
+            // Arrange
+            var expressionMock = new Mock<Expression>().Object;
+
+            // Act
+            var inMemoryAsyncEnumerable = new InMemoryAsyncEnumerable<int>(expressionMock);
+
+            // Assert
+            Assert.Equal(expressionMock, ((IQueryable)inMemoryAsyncEnumerable).Expression);
+        }
+    }
+}
---
+++ src/Moq.EntityFramework6.Helpers.Tests/DbAsyncQueryProvider/InMemoryDbAsyncEnumeratorTests.cs
@@ -0,0 +1,59 @@
+﻿namespace Moq.EntityFramework6.Helpers.Tests.DbAsyncQueryProvider
+{
+    using System.Collections.Generic;
+    using System.Data.Entity.Infrastructure;
+    using System.Threading;
+    using Helpers.DbAsyncQueryProvider;
+    using Xunit;
+
+    public class InMemoryDbAsyncEnumeratorTests
+    {
+        private readonly Mock<IEnumerator<int>> enumeratorMock = new Mock<IEnumerator<int>>();
+        private readonly InMemoryDbAsyncEnumerator<int> inMemoryDbAsyncEnumerator;
+
+        public InMemoryDbAsyncEnumeratorTests()
+        {
+            this.inMemoryDbAsyncEnumerator = new InMemoryDbAsyncEnumerator<int>(this.enumeratorMock.Object);
+        }
+
+        [Fact]
+        public void Given_InMemoryDbAsyncEnumerator_When_Dispose_Then_InnerEnumeratorShouldBeDisposed()
+        {
+            // Act
+            this.inMemoryDbAsyncEnumerator.Dispose();
+
+            // Assert
+            this.enumeratorMock.Verify(x => x.Dispose());
+        }
+
+        [Fact]
+        public void Given_InMemoryDbAsyncEnumerator_When_Current_Then_CurrentFromInInnerEnumeratorShouleBeUsed()
+        {
+            // Act
+            int result = this.inMemoryDbAsyncEnumerator.Current;
+
+            // Assert
+            this.enumeratorMock.VerifyGet(x=> x.Current);
+        }
+
+        [Fact]
+        public void Given_InMemoryDbAsyncEnumerator_When_CurrentFromInterface_Then_CurrentFromInInnerEnumeratorShouleBeUsed()
+        {
+            // Act
+            object result = ((IDbAsyncEnumerator)this.inMemoryDbAsyncEnumerator).Current;
+
+            // Assert
+            this.enumeratorMock.VerifyGet(x => x.Current);
+        }
+
+        [Fact]
+        public async void Given_InMemoryDbAsyncEnumerator_When_Current_Then_CurrentShouldBeSameAsInInnerEnumerator()
+        {
+            // Act
+            await this.inMemoryDbAsyncEnumerator.MoveNextAsync(CancellationToken.None);
+
+            // Assert
+            this.enumeratorMock.Verify(x => x.MoveNext());
+        }
+    }
+}
